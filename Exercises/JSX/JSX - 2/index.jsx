d6aab7d6cf47e5ac902d9f091369d2ee3e58e71b
Karam Guliyev <karamguliyev10@gmail.com>
2023-03-12 00:15:12 +0400

    Fixes

--- Exercises/JSX/JSX - 2/index.jsx
+++
@@ -1,6 +0,0 @@
-// Write a function that receives a name argument and returns a h1 tag with the name variable appended to the "Hello, " string.
-
-
-function name(yourName) {
-    return <h1>Hello {yourName}</h1>
-}
--- Exercises/JSX/JSX - 3/index.jsx
+++
@@ -1,5 +0,0 @@
-// Write a function that receives two parameters, a and b and returns the sum of them within an h2 tag.
-
-function twoParam(first, second) {
-    return <h2>{first + second}</h2>
-}
