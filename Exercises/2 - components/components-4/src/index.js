8c42228fd948008023ff89bbe170c9efea656347
Karam Guliyev <karamguliyev10@gmail.com>
2023-03-12 00:34:30 +0400

    Components - 4

---
+++ Exercises/2 - components/components-4/src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom/client";
+import { RenderApp } from "./App";
+
+// Create a new index.js file within the src folder and render the App component using the ReactDOM.render method.
+// I think I already did this? in a kinnnda different way...? 
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RenderApp />);
