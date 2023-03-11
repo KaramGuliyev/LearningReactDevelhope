fc753e20adcb41b48c1fdbaa5d383861a0a51cfd
Karam Guliyev <karamguliyev10@gmail.com>
2023-03-12 00:12:53 +0400

    Components - 2

---
+++ Exercises/2 - components/components-2/src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom/client";
+import { Hello } from "./App";
+
+// Write a Hello class components that renders the "Hello, World!" message within an h1 tag.
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Hello />);
