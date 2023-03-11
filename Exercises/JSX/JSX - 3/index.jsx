277e0964bfa61f2ded30f0041fceaca3d15ec336
Karam Guliyev <karamguliyev10@gmail.com>
2023-03-11 23:04:04 +0400

    JSX - 3

---
+++ Exercises/JSX/JSX - 3/index.jsx
@@ -0,0 +1,5 @@
+// Write a function that receives two parameters, a and b and returns the sum of them within an h2 tag.
+
+function twoParam(first, second) {
+    return <h2>{first + second}</h2>
+}
